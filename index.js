0435eb4ce71392469a0e88304783dde0c4204ffa
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-07-15 17:56:42 -0400

    entering data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+const Person = require('./models/Person')
+
 // forma de ler JSON
 app.use(
     express.urlencoded({
@@ -11,6 +13,36 @@ app.use(
 )
 
 app.use(express.json())
+
+// rotas da API
+app.post('/person', async (req, res) => {
+
+    // req.body
+
+    // {name: "Raphael", salary: 5000, approved: false}
+    const {name, salary, approved} = req.body
+
+    if(!name) {
+        res.status(422).json({error: 'O nome é obrigatório!'})
+    }
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    try {
+        // criando dados
+        await Person.create(person)
+
+        res.status(201).json({message: 'Pessoa inserida no sistema com sucesso!'})
+
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+
+})
 
 // rota inicial / endpoint
 app.get('/', (req, res) => {
